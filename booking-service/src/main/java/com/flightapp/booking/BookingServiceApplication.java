e6510f02cd8b01cd6e37f2f3d34de6eed809e901
QuantumSoham <quantumsc03@gmail.com>
2025-12-01 11:06:28 +0530

    eureka server and booking server added

--- booking-service/src/main/java/com/flightapp/booking/BookingServiceApplication.java
+++ booking-service/src/main/java/com/flightapp/booking/BookingServiceApplication.java
@@ -1,14 +1,17 @@
-package com.flightapp.flight;
+package com.flightapp.booking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class FlightServiceApplication {
+@ComponentScan(basePackages = {"com.flightapp.booking"})
+
+public class BookingServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FlightServiceApplication.class, args);
+        SpringApplication.run(BookingServiceApplication.class,   args);
     }
 }
