68064de5546992b59b2ba3d9470794617af2ae87
QuantumSoham <quantumsc03@gmail.com>
2025-12-01 20:59:59 +0530

    Load balencer added & open feign added

--- booking-service/src/main/java/com/flightapp/booking/BookingServiceApplication.java
+++ booking-service/src/main/java/com/flightapp/booking/BookingServiceApplication.java
@@ -3,10 +3,12 @@ package com.flightapp.booking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.flightapp.booking.client")
 @ComponentScan(basePackages = {"com.flightapp.booking"})
 
 public class BookingServiceApplication {
